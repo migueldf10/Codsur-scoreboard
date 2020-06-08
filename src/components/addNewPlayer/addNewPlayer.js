45d3a50e07924c2cc25b897f087a4b0e27eff80b
migueldf10 <migueldf10@gmail.com>
2020-06-08 18:21:11 +0200

    Removing Form and keeping only input. Forms are messy

--- src/components/addNewPlayer/addNewPlayer.js
+++ src/components/addNewPlayer/addNewPlayer.js
@@ -2,26 +2,14 @@ import React, { useState } from 'react'
 
 export default function AddNewPlayer(props) {
 	const [newPlayerName, setNewPlayerName] = useState("")
-
-
 	const handleChange = (event) => {
-		console.log(event.target.value)
 		setNewPlayerName(event.target.value)
-		event.preventDefault();
 	}
-
-	const handleSubmit = (event) => {
-		addNewPlayer(newPlayerName)
-		event.preventDefault();
-	}
-
 	const { addNewPlayer } = props
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<input onChange={handleChange} />
-				<input type="submit" value="Submit" />
-			</form>
+			<input onChange={handleChange} />
+			<button onClick={() => addNewPlayer(newPlayerName)}>Add Player</button>
 		</div>
 	)
 }
