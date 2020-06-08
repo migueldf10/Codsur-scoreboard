c6155c44f3ff583fb8841c56c751b220161f0675
migueldf10 <migueldf10@gmail.com>
2020-06-08 19:36:02 +0200

    renive player id (debugging useless now)

--- src/components/playerGridItem/playerGridItem.js
+++ src/components/playerGridItem/playerGridItem.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import './playerGridItem.scss'
 export default function PlayerGridItem(props) {
-	const { player, increaseScore, index, winnerScore } = props
+	const { player, increaseScore, winnerScore } = props
 	return (
 		<div className='player-grid-item'>
-			<span>{player.id}</span>
 			<h3>{player.name}</h3>
 			<span>Score: {player.score}</span>
 			<button onClick={() => increaseScore(player)}>+</button>
